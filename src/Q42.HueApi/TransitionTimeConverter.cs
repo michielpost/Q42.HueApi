ffa161aa643c15aebe5ebf4dc9e9cee284991082
ermau <me@ermau.com>
2013-01-19 20:39:36 -0500

    Fix TimeSpan converter

--- src/Q42.HueApi/TransitionTimeConverter.cs
+++ src/Q42.HueApi/TransitionTimeConverter.cs
@@ -3,18 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Q42.HueApi
 {
   internal class TransitionTimeConverter
-    : Newtonsoft.Json.Converters.CustomCreationConverter<TimeSpan?>
+    : JsonConverter
   {
-    public override TimeSpan? Create (Type objectType)
+    public override bool CanConvert(Type objectType)
     {
-      return new TimeSpan();
+      return objectType == typeof (TimeSpan?) || objectType == typeof (TimeSpan);
     }
 
-    public override object ReadJson(Newtonsoft.Json.JsonReader reader, Type objectType, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
+    public override bool CanRead
+    {
+      get { return true; }
+    }
+
+    public override bool CanWrite
+    {
+      get { return true; }
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
     {
       int? value = reader.ReadAsInt32();
       if (value == null)
@@ -23,13 +34,22 @@ namespace Q42.HueApi
       return (TimeSpan?)TimeSpan.FromMilliseconds (value.Value * 100);
     }
 
-    public override void WriteJson(Newtonsoft.Json.JsonWriter writer, object value, Newtonsoft.Json.JsonSerializer serializer)
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
     {
-      TimeSpan? ts = (TimeSpan?)value;
-      if (ts == null)
-        writer.WriteValue ((int?)null);
+      TimeSpan span;
+
+      if (value == null)
+      {
+          writer.WriteValue (0);
+          return;
+      }
+
+      if (value is TimeSpan?)
+        span = ((TimeSpan?)value).Value;
       else
-        writer.WriteValue((int?)(ts.Value.TotalSeconds * 10));
+        span = (TimeSpan)value;
+
+      writer.WriteValue ((int?)(span.TotalSeconds * 10));
     }
   }
 }
