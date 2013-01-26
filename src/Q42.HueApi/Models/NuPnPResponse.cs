8dc67c6b674d1cbf67505d09d3a4482546dbec61
ermau <me@ermau.com>
2013-01-26 13:55:05 -0500

    NuPnPResponse isn't used by user, should be internal

--- src/Q42.HueApi/Models/NuPnPResponse.cs
+++ src/Q42.HueApi/Models/NuPnPResponse.cs
@@ -9,7 +9,7 @@ namespace Q42.HueApi.Models
   /// <summary>
   /// Model for response from http://www.meethue.com/api/nupnp
   /// </summary>
-  public class NuPnPResponse
+  internal class NuPnPResponse
   {
     public string Id { get; set; }
     public string InternalIpAddress { get; set; }
