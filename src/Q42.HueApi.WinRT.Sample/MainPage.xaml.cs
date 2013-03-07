b7c163d11f2b01e32e8a3b3e238f7113587a9e72
michielpost <michiel@q42.nl>
2013-03-07 16:10:43 +0100

    Added basic Windows 8 sample app to show HueApi functions

---
+++ src/Q42.HueApi.WinRT.Sample/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Q42.HueApi.WinRT.Sample.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Q42.HueApi.WinRT.Sample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+				private void LocateBridgeButton_Click(object sender, RoutedEventArgs e)
+				{
+					NavFrame.Navigate(typeof(LocateBridgePage));
+
+				}
+
+				private void RegisterButton_Click(object sender, RoutedEventArgs e)
+				{
+					NavFrame.Navigate(typeof(RegisterPage));
+
+				}
+
+				private void FindLightsButton_Click(object sender, RoutedEventArgs e)
+				{
+					NavFrame.Navigate(typeof(FindLightsPage));
+
+				}
+
+				private void LightCommandsButton_Click(object sender, RoutedEventArgs e)
+				{
+					NavFrame.Navigate(typeof(LightCommandsPage));
+
+				}
+    }
+}
---
+++ src/Q42.HueApi.WinRT.Sample/ViewModel/MainViewModel.cs
@@ -0,0 +1,211 @@
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Q42.HueApi.Interfaces;
+using Q42.WinRT.Data;
+using System;
+using System.Linq;
+
+namespace Q42.HueApi.WinRT.Sample.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+			public DataLoader LocateBridgeDataLoader { get; set; }
+			public RelayCommand LocateBridgeCommand { get; set; }
+
+			public DataLoader SsdpLocateBridgeDataLoader { get; set; }
+			public RelayCommand SsdpLocateBridgeCommand { get; set; }
+
+			public DataLoader RegisterDataLoader { get; set; }
+
+
+			public RelayCommand GetLightsCommand { get; set; }
+
+			public RelayCommand TurnOnCommand { get; set; }
+			public RelayCommand TurnOffCommand { get; set; }
+			public RelayCommand GreenCommand { get; set; }
+			public RelayCommand RedCommand { get; set; }
+			public RelayCommand ColorloopCommand { get; set; }
+
+
+
+			IBridgeLocator httpLocator = new HttpBridgeLocator();
+			IBridgeLocator ssdpLocator = new SSDPBridgeLocator();
+
+			HueClient _hueClient;
+
+
+			private string _httpBridges;
+
+			public string HttpBridges
+			{
+				get { return _httpBridges; }
+				set
+				{
+					_httpBridges = value;
+					RaisePropertyChanged("HttpBridges");
+				}
+			}
+
+			private string _ssdpBridges;
+
+			public string SsdpBridges
+			{
+				get { return _ssdpBridges; }
+				set
+				{
+					_ssdpBridges = value;
+					RaisePropertyChanged("SsdpBridges");
+				}
+			}
+
+			private bool _registerSuccess;
+
+			public bool RegisterSuccess
+			{
+				get { return _registerSuccess; }
+				set { _registerSuccess = value;
+				RaisePropertyChanged("RegisterSuccess");
+				}
+			}
+
+			private string _getLights;
+
+			public string GetLights
+			{
+				get { return _getLights; }
+				set
+				{
+					_getLights = value;
+					RaisePropertyChanged("GetLights");
+				}
+			}
+
+
+			
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+					LocateBridgeDataLoader = new DataLoader();
+					SsdpLocateBridgeDataLoader = new DataLoader();
+
+					RegisterDataLoader = new DataLoader();
+
+
+					LocateBridgeCommand = new RelayCommand(LocateBridgeAction);
+					SsdpLocateBridgeCommand = new RelayCommand(SsdpLocateBridgeAction);
+
+					GetLightsCommand = new RelayCommand(GetLightsAction);
+
+					TurnOnCommand = new RelayCommand(TurnOnAction);
+					TurnOffCommand = new RelayCommand(TurnOffAction);
+					GreenCommand = new RelayCommand(GreenAction);
+					RedCommand = new RelayCommand(RedAction);
+					ColorloopCommand = new RelayCommand(ColorloopAction);
+        }
+
+				
+				private async void LocateBridgeAction()
+				{
+					var result = await LocateBridgeDataLoader.LoadAsync(() => httpLocator.LocateBridgesAsync(TimeSpan.FromSeconds(5)));
+
+					HttpBridges = string.Join(", ", result.ToArray());
+
+					if (result.Count() > 0)
+						_hueClient = new HueClient(result.First());
+				}
+
+				private async void SsdpLocateBridgeAction()
+				{
+					var result = await SsdpLocateBridgeDataLoader.LoadAsync(() => ssdpLocator.LocateBridgesAsync(TimeSpan.FromSeconds(5)));
+
+					SsdpBridges = string.Join(", ", result.ToArray());
+
+					if (result.Count() > 0)
+						_hueClient = new HueClient(result.First());
+				}
+
+				internal async void Register(string p)
+				{
+					var result = await RegisterDataLoader.LoadAsync(() => _hueClient.RegisterAsync(p, p));
+
+					RegisterSuccess = result;
+				}
+
+				internal void Initialize(string p)
+				{
+					_hueClient.Initialize(p);
+				}
+
+
+				private async void GetLightsAction()
+				{
+					var result = await _hueClient.GetLightsAsync();
+
+					GetLights = string.Format("Found {0} lights", result.Count());
+				}
+
+
+				private void RedAction()
+				{
+					LightCommand command = new LightCommand();
+					command.TurnOn().SetColor("FF0000");
+
+					_hueClient.SendCommandAsync(command);
+				}
+
+				private void GreenAction()
+				{
+					LightCommand command = new LightCommand();
+					command.TurnOn().SetColor("00FF00");
+
+					_hueClient.SendCommandAsync(command);
+				}
+
+				private void TurnOffAction()
+				{
+					LightCommand command = new LightCommand();
+					command.TurnOff();
+
+					_hueClient.SendCommandAsync(command);
+				}
+
+				private void TurnOnAction()
+				{
+					LightCommand command = new LightCommand();
+					command.TurnOn();
+
+					_hueClient.SendCommandAsync(command);
+				}
+
+				private void ColorloopAction()
+				{
+					LightCommand command = new LightCommand();
+					command.TurnOn();
+					command.Effect = Effect.ColorLoop;
+
+					_hueClient.SendCommandAsync(command);
+				}
+
+
+
+				internal void ManualRegister(string ip)
+				{
+					_hueClient = new HueClient(ip);
+				}
+		}
+}
