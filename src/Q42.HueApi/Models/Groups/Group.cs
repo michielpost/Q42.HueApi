eb377148bafd3782f9eba35dcf6266a4b9d6285e
michielpost <michiel@q42.nl>
2013-10-31 16:28:50 +0100

    Get all groups

---
+++ src/Q42.HueApi/Models/Groups/Group.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q42.HueApi.Models.Groups
+{
+  public class Group
+  {
+    public string Id { get; set; }
+    public string Name { get; set; }
+  }
+}
