3380c8f6c31190dd45febd058eb69d1e41c1eeb2
Koen van der Linden <vanderlinden.koen@gmail.com>
2014-08-29 11:17:23 +0200

    Bridge config changes to fully support API version 1.3.0

---
+++ src/Q42.HueApi/Models/PortalState.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace Q42.HueApi
+{
+  [DataContract]
+  public class PortalState
+  {
+    [DataMember(Name = "signedon")]
+    public bool SignedOn { get; set; }
+
+    [DataMember(Name = "incoming")]
+    public bool Incoming { get; set; }
+
+    [DataMember(Name = "outgoing")]
+    public bool Outgoing { get; set; }
+
+    [DataMember(Name = "communication")]
+    public string Communication { get; set; }
+  }
+}
