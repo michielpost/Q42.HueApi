d72610bdae7d6ad289c8dd4cda5daebf2dc1e2f0
Michiel Post <michiel@q42.nl>
2014-12-09 15:46:09 +0100

    Software Update properties added for bridge 1.4 update

--- src/Q42.HueApi/Models/SoftwareUpdate.cs
+++ src/Q42.HueApi/Models/SoftwareUpdate.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace Q42.HueApi
 {
@@ -23,5 +24,22 @@ namespace Q42.HueApi
 
     [DataMember (Name = "notify")]
     public bool Notify { get; set; }
+
+    /// <summary>
+    /// Setting this flag to true lets the bridge search for software updates in the portal. After the search attempt, this flag is set back to false. Requires portal connection to update server
+    /// </summary>
+    [DataMember(Name = "checkforupdate")]
+    public bool CheckForUpdate { get; set; }
+  }
+
+  [DataContract]
+  public class SoftwareUpdateDevices
+  {
+    [DataMember(Name = "bridge")]
+    public bool Bridge { get; set; }
+
+    [DataMember(Name = "lights")]
+    public IReadOnlyCollection<string> Lights { get; set; }
+
   }
 }
