7d5301e5081d820a3fa85326546d1832e581ce8e
Michiel Post <michiel@q42.nl>
2017-04-28 16:59:05 +0200

    Converting to .Net Standard 1.1

--- src/Q42.HueApi.UniversalWindows.Sample/Properties/AssemblyInfo.cs
+++ src/Q42.HueApi.UniversalWindows.Sample/Properties/AssemblyInfo.cs
@@ -1,20 +1,18 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Q42.HueApi")]
+[assembly: AssemblyTitle("Q42.HueApi.UniversalWindows.Sample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Q42.HueApi")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("Q42.HueApi.UniversalWindows.Sample")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -28,6 +26,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("Q42.HueApi.Tests")]
-[assembly: InternalsVisibleTo("Q42.HueApi.NET.Tests")]
+[assembly: ComVisible(false)]
