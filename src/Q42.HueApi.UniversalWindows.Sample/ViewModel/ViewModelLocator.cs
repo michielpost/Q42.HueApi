08125eb36f77155bcfed40b3475ec386c107b4fe
Michiel Post <michiel@q42.nl>
2018-08-10 22:04:11 +0200

    Q42.HueApi version 3.7.0

--- src/Q42.HueApi.UniversalWindows.Sample/ViewModel/ViewModelLocator.cs
+++ src/Q42.HueApi.UniversalWindows.Sample/ViewModel/ViewModelLocator.cs
@@ -12,9 +12,9 @@
   See http://www.galasoft.ch/mvvm
 */
 
+using CommonServiceLocator;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
-using Microsoft.Practices.ServiceLocation;
 
 namespace Q42.HueApi.UniversalWindows.Sample.ViewModel
 {
