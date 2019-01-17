7d21951ae3135f9e46aa13a81d747ed7962fa1a7
Philippe Wechsler <myself@philippewechsler.ch>
2019-01-17 18:02:11 +0100

    fixed typo UpdateroupRequest -> UpdateGroupRequest

--- src/Q42.HueApi/Models/Groups/CreateGroupRequest.cs
+++ src/Q42.HueApi/Models/Groups/CreateGroupRequest.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace Q42.HueApi.Models.Groups
 {
   [DataContract]
-  internal class CreateGroupRequest : UpdateroupRequest
+  internal class CreateGroupRequest : UpdateGroupRequest
   {
 
     /// <summary>
