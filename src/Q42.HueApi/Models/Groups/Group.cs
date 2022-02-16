1b13bc120fd52060ee48e3a385da0e82b2af8082
michielpost <michiel@q42.nl>
2022-02-16 11:06:07 +0100

    Added RoomClass Free for #270

--- src/Q42.HueApi/Models/Groups/Group.cs
+++ src/Q42.HueApi/Models/Groups/Group.cs
@@ -11,48 +11,48 @@ using System.Threading.Tasks;
 
 namespace Q42.HueApi.Models.Groups
 {
-	[DataContract]
-	public class Group
-	{
-		public string Id { get; set; }
+  [DataContract]
+  public class Group
+  {
+    public string Id { get; set; }
 
-		[DataMember(Name = "name")]
-		public string Name { get; set; }
+    [DataMember(Name = "name")]
+    public string Name { get; set; }
 
-		/// <summary>
-		/// Luminaire / Lightsource / LightGroup
-		/// </summary>
-		[JsonConverter(typeof(StringNullableEnumConverter))]
-		[DataMember(Name = "type")]
-		public GroupType? Type { get; set; }
+    /// <summary>
+    /// Luminaire / Lightsource / LightGroup
+    /// </summary>
+    [JsonConverter(typeof(StringNullableEnumConverter))]
+    [DataMember(Name = "type")]
+    public GroupType? Type { get; set; }
 
-		/// <summary>
-		/// Category of the Room type. Default is "Other".
-		/// </summary>
-		[JsonConverter(typeof(StringNullableEnumConverter))]
-		[DataMember(Name = "class")]
-		public RoomClass? Class { get; set; }
+    /// <summary>
+    /// Category of the Room type. Default is "Other".
+    /// </summary>
+    [JsonConverter(typeof(StringNullableEnumConverter))]
+    [DataMember(Name = "class")]
+    public RoomClass? Class { get; set; }
 
-		/// <summary>
-		/// As of 1.4. Uniquely identifies the hardware model of the luminaire. Only present for automatically created Luminaires.
-		/// </summary>
-		[DataMember(Name = "modelid")]
-		public string ModelId { get; set; }
+    /// <summary>
+    /// As of 1.4. Uniquely identifies the hardware model of the luminaire. Only present for automatically created Luminaires.
+    /// </summary>
+    [DataMember(Name = "modelid")]
+    public string ModelId { get; set; }
 
-		/// <summary>
-		/// The IDs of the lights that are in the group.
-		/// </summary>
-		[DataMember(Name = "lights")]
-		public List<string> Lights { get; set; }
+    /// <summary>
+    /// The IDs of the lights that are in the group.
+    /// </summary>
+    [DataMember(Name = "lights")]
+    public List<string> Lights { get; set; }
 
-		/// <summary>
-		/// The light state of one of the lamps in the group.
-		/// </summary>
-		[DataMember(Name = "action")]
-		public State Action { get; set; }
+    /// <summary>
+    /// The light state of one of the lamps in the group.
+    /// </summary>
+    [DataMember(Name = "action")]
+    public State Action { get; set; }
 
-		[DataMember(Name = "state")]
-		public GroupState State { get; set; }
+    [DataMember(Name = "state")]
+    public GroupState State { get; set; }
 
     [DataMember(Name = "locations")]
     public Dictionary<string, LightLocation> Locations { get; set; }
@@ -134,7 +134,7 @@ namespace Q42.HueApi.Models.Groups
     [JsonIgnore]
     public double X
     {
-      get { return this[0];  }
+      get { return this[0]; }
       set { this[0] = value; }
     }
 
@@ -177,7 +177,7 @@ namespace Q42.HueApi.Models.Groups
     /// <summary>
     /// X > -0.1 && X < 0.1
     /// </summary>
-    public bool IsCenter => X > -0.1 && X < 0.1 ;
+    public bool IsCenter => X > -0.1 && X < 0.1;
 
     public double Distance(double x, double y, double z)
     {
@@ -199,78 +199,80 @@ namespace Q42.HueApi.Models.Groups
   }
 
   [DataContract]
-	public class GroupState
-	{
-		[DataMember(Name = "any_on")]
-		public bool? AnyOn { get; set; }
+  public class GroupState
+  {
+    [DataMember(Name = "any_on")]
+    public bool? AnyOn { get; set; }
 
-		[DataMember(Name = "all_on")]
-		public bool? AllOn { get; set; }
-	}
+    [DataMember(Name = "all_on")]
+    public bool? AllOn { get; set; }
+  }
 
-	/// <summary>
-	/// Possible group types
-	/// </summary>
-	public enum GroupType
-	{
-		
-		[EnumMember(Value = "LightGroup")]
-		LightGroup,
-		[EnumMember(Value = "Room")]
-		Room,
-		[EnumMember(Value = "Luminaire")]
-		Luminaire,
-		[EnumMember(Value = "LightSource")]
-		LightSource,
+  /// <summary>
+  /// Possible group types
+  /// </summary>
+  public enum GroupType
+  {
+
+    [EnumMember(Value = "LightGroup")]
+    LightGroup,
+    [EnumMember(Value = "Room")]
+    Room,
+    [EnumMember(Value = "Luminaire")]
+    Luminaire,
+    [EnumMember(Value = "LightSource")]
+    LightSource,
     [EnumMember(Value = "Entertainment")]
     Entertainment,
     [EnumMember(Value = "Zone")]
-    Zone
+    Zone,
+    [EnumMember(Value = "Free")]
+    Free
   }
 
-	/// <summary>
-	/// Possible room types
-	/// </summary>
-	public enum RoomClass
-	{
-		[EnumMember(Value = "Other")]
-		Other,
-		[EnumMember(Value = "Living room")]
-		LivingRoom,
-		[EnumMember(Value = "Kitchen")]
-		Kitchen,
-		[EnumMember(Value = "Dining")]
-		Dining,
-		[EnumMember(Value = "Bedroom")]
-		Bedroom,
-		[EnumMember(Value = "Kids bedroom")]
-		KidsBedroom,
-		[EnumMember(Value = "Bathroom")]
-		Bathroom,
-		[EnumMember(Value = "Nursery")]
-		Nursery,
-		[EnumMember(Value = "Recreation")]
-		Recreation,
-		[EnumMember(Value = "Office")]
-		Office,
-		[EnumMember(Value = "Gym")]
-		Gym,
-		[EnumMember(Value = "Hallway")]
-		Hallway,
-		[EnumMember(Value = "Toilet")]
-		Toilet,
-		[EnumMember(Value = "Front door")]
-		FrontDoor,
-		[EnumMember(Value = "Garage")]
-		Garage,
-		[EnumMember(Value = "Terrace")]
-		Terrace,
-		[EnumMember(Value = "Garden")]
-		Garden,
-		[EnumMember(Value = "Driveway")]
-		Driveway,
-		[EnumMember(Value = "Carport")]
-		Carport,
+  /// <summary>
+  /// Possible room types
+  /// </summary>
+  public enum RoomClass
+  {
+    [EnumMember(Value = "Other")]
+    Other,
+    [EnumMember(Value = "Living room")]
+    LivingRoom,
+    [EnumMember(Value = "Kitchen")]
+    Kitchen,
+    [EnumMember(Value = "Dining")]
+    Dining,
+    [EnumMember(Value = "Bedroom")]
+    Bedroom,
+    [EnumMember(Value = "Kids bedroom")]
+    KidsBedroom,
+    [EnumMember(Value = "Bathroom")]
+    Bathroom,
+    [EnumMember(Value = "Nursery")]
+    Nursery,
+    [EnumMember(Value = "Recreation")]
+    Recreation,
+    [EnumMember(Value = "Office")]
+    Office,
+    [EnumMember(Value = "Gym")]
+    Gym,
+    [EnumMember(Value = "Hallway")]
+    Hallway,
+    [EnumMember(Value = "Toilet")]
+    Toilet,
+    [EnumMember(Value = "Front door")]
+    FrontDoor,
+    [EnumMember(Value = "Garage")]
+    Garage,
+    [EnumMember(Value = "Terrace")]
+    Terrace,
+    [EnumMember(Value = "Garden")]
+    Garden,
+    [EnumMember(Value = "Driveway")]
+    Driveway,
+    [EnumMember(Value = "Carport")]
+    Carport,
     [EnumMember(Value = "TV")]
     TV,
     [EnumMember(Value = "Home")]
@@ -312,19 +314,21 @@ namespace Q42.HueApi.Models.Groups
     [EnumMember(Value = "Barbecue")]
     Barbecue,
     [EnumMember(Value = "Pool")]
-    Pool
+    Pool,
+    [EnumMember(Value = "Free")]
+    Free
   }
 
   [DataContract]
   public class Stream
   {
-		[DataMember(Name = "proxymode")]
+    [DataMember(Name = "proxymode")]
     public string ProxyMode { get; set; }
-		[DataMember(Name = "proxynode")]
+    [DataMember(Name = "proxynode")]
     public string ProxyNode { get; set; }
-		[DataMember(Name = "active")]
+    [DataMember(Name = "active")]
     public bool Active { get; set; }
-		[DataMember(Name = "owner")]
+    [DataMember(Name = "owner")]
     public string Owner { get; set; }
   }
 
