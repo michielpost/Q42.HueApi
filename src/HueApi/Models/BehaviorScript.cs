3fc75a4d4b0d2a8dff464fb6582d069d40d17327
Michiel Post <michiel@michielpost.nl>
2023-06-26 13:03:46 +0200

    Fix for #309

--- src/HueApi/Models/BehaviorScript.cs
+++ src/HueApi/Models/BehaviorScript.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
@@ -13,13 +14,13 @@ namespace HueApi.Models
     public string Description { get; set; } = default!;
 
     [JsonPropertyName("configuration_schema")]
-    public string ConfigurationSchema { get; set; } = default!;
+    public JsonElement ConfigurationSchema { get; set; } = default!;
 
     [JsonPropertyName("trigger_schema")]
-    public string TriggerSchema { get; set; } = default!;
+    public JsonElement TriggerSchema { get; set; } = default!;
 
     [JsonPropertyName("state_schema")]
-    public string StateSchema { get; set; } = default!;
+    public JsonElement StateSchema { get; set; } = default!;
 
     [JsonPropertyName("version")]
     public string Version { get; set; } = default!;
